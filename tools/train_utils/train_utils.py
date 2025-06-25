f99ef19e01978a213cf810241cd1ca49cd1c6f75
veerain <veerain@etid-c4gjz84.engr.tamu.edu>
2025-06-25 16:29:18 -0500

    Removed redundant spconv and cumm folders

--- tools/train_utils/train_utils.py
+++ tools/train_utils/train_utils.py
@@ -157,8 +157,7 @@ def train_model(model, optimizer, train_loader, model_func, lr_scheduler, optim_
     # use for disable data augmentation hook
     hook_config = cfg.get('HOOK', None) 
     augment_disable_flag = False
-    total_epochs = 1
-    start_epoch = 1
+
     with tqdm.trange(start_epoch, total_epochs, desc='epochs', dynamic_ncols=True, leave=(rank == 0)) as tbar:
         total_it_each_epoch = len(train_loader)
         if merge_all_iters_to_one_epoch:
